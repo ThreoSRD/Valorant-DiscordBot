ffcbf2751d0cc63457b0558fb6024ffe4fd40fe2
小闰德 <89167254+ThreoSRD@users.noreply.github.com>
2023-01-07 15:43:13 -0500

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -51,31 +51,15 @@ class Admin(commands.Cog):
     async def about(self, interaction: Interaction) -> None:
         """Shows basic information about the bot."""
 
-        owner_url = f'https://discord.com/users/240059262297047041'
+        owner_url = f'https://discord.com/users/391531631925526528'
         github_project = 'https://github.com/staciax/Valorant-DiscordBot'
         support_url = 'https://discord.gg/FJSXPqQZgz'
 
         embed = discord.Embed(color=0xFFFFFF)
         embed.set_author(name='VALORANT BOT PROJECT', url=github_project)
         embed.set_thumbnail(url='https://i.imgur.com/ZtuNW0Z.png')
-        embed.add_field(name='DEV:', value=f"[ꜱᴛᴀᴄɪᴀ.#7475]({owner_url})", inline=False)
-        embed.add_field(
-            name='ᴄᴏɴᴛʀɪʙᴜᴛᴏʀꜱ:',
-            value=f"[kiznick](https://github.com/kiznick)\n"
-            "[KANATAISGOD](https://github.com/KANATAISGOD)\n"
-            "[TMADZ2007](https://github.com/KANATAISGOD')\n"
-            "[sevzin](https://github.com/sevzin)\n"
-            "[miigoxyz](https://github.com/miigoxyz)\n"
-            "[Connor](https://github.com/ConnorDoesDev)\n"
-            "[KohanaSann](https://github.com/KohanaSann)\n"
-            "[RyugaXhypeR](https://github.com/RyugaXhypeR)\n"
-            "[Austin Hornhead](https://github.com/marchingon12)\n",
-            inline=False,
-        )
+        embed.add_field(name='DEV:', value=f"[Threo#0515]({owner_url})", inline=False)
         view = ui.View()
-        view.add_item(ui.Button(label='GITHUB', url=github_project, row=0))
-        view.add_item(ui.Button(label='KO-FI', url='https://ko-fi.com/staciax', row=0))
-        view.add_item(ui.Button(label='SUPPORT SERVER', url=support_url, row=0))
 
         await interaction.response.send_message(embed=embed, view=view)
 
